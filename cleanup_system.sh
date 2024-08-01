67773f0d0f279af2677564029ec37fd0b6c71513
majobasgall <majobasgall@users.noreply.github.com>
2024-08-01 15:07:26 +0200

    add home cleaner by subdirectories

--- cleanup_system.sh
+++ cleanup_system.sh
@@ -105,6 +105,52 @@ clean_docker_component() {
 }
 
 
+# Function to display disk usage in the home directory excluding a specified directory
+inspect_and_clean_home_subdir() {
+    show_disk_usage() {
+        echo "Analyzing disk usage in /home, excluding $EXCLUDE_DIR..."
+        du -ah --exclude=/home/$EXCLUDE_DIR /home | sort -rh | head -n 30
+    }
+
+    # Prompt user for a directory to exclude from the disk usage analysis
+    read -p "Enter the path relative to your home directory to exclude (e.g., $USER/0000): " EXCLUDE_DIR
+
+    while true; do
+        show_disk_usage
+
+        # Ask if the user wants to delete any directory
+        read -p "Do you want to delete any directory? (Y/n): " DECIDE
+
+        if [ "$DECIDE" != "Y" ]; then
+            echo "Exiting."
+            break
+        fi
+
+        read -p "Copy the path of the directory you want to delete: " DELETE_PATH
+
+	# Confirm the deletion
+	read -p "Are you sure you want to delete $DELETE_PATH? This action cannot be undone. (Y/n): " CONFIRM
+
+	if [ "$CONFIRM" == "Y" ]; then
+	    rm -rf $DELETE_PATH
+	    echo "$DELETE_PATH has been deleted."
+	else
+	    echo "Deletion cancelled."
+	fi
+
+	# Show disk usage after deletion
+	show_disk_usage
+
+	# Ask if the user wants to delete another directory or return to the calling code
+        read -p "Do you want to delete another directory? (Y/n): " CONTINUE
+
+        if [ "$CONTINUE" != "Y" ]; then
+            break
+        fi
+    done
+}
+
+
 while true; do
     echo "Release Space System"
     echo "0. Clean up Pacman Cache"
@@ -113,9 +159,10 @@ while true; do
     echo "3. Remove Orphaned Packages"
     echo "4. Show /var/log Size and Remove Old Logs"
     echo "5. Clean up Docker components"
-    echo "6. Quit"
+    echo "6. Inspect and Clean /home Subdirs"
+    echo "7. Quit"
 
-    read -p "Select an option (0/1/2/3/4/5/6): " choice
+    read -p "Select an option (0/1/2/3/4/5/6/7): " choice
 
     case $choice in
         0)
@@ -140,6 +187,9 @@ while true; do
 	    docker system df
             ;;
         6)
+            inspect_and_clean_home_subdir
+            ;;
+        7)
             echo "Exiting..."
             exit 0
             ;;
